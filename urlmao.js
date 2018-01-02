50442831647d1806d56ac50e71259e4763d64545
f1ux1uxday <fluxpatron@gmail.com>
2018-01-01 18:32:43 -0600

    hooked input field up to submit button href

--- urlmao.js
+++ urlmao.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const nofavicon = require('express-no-favicons')
 
 const app = express()
 
 app.disable('x-powered-by')
 app.use(cors())
+app.use(nofavicon())
 app.use(express.static(__dirname + '/static'))
 
 app.get('/:urlParam(*)', (request, response) => {
 
   let urlParam = request.params.urlParam
 
+  console.log(urlParam)
   response.json({
     url: urlParam,
   })
