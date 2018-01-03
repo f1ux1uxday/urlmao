f49a0fe3f1f62d0274f047c7ad42a8226f2db066
f1ux1uxday <fluxpatron@gmail.com>
2018-01-03 10:34:15 -0600

    added shortener

--- urlmao.js
+++ urlmao.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const nofavicon = require('express-no-favicons')
+const shortener = require('./shortener')
 
 const app = express()
 
