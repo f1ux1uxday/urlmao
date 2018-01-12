606efb4cb6dbdc22063062a992bb17c086746082
f1ux1uxday <fluxpatron@gmail.com>
2018-01-12 15:21:01 -0600

    trimming the unnecessary

--- urlmao.js
+++ urlmao.js
@@ -32,9 +32,6 @@ app.get('/url/:urlParam(*)', (request, response) => {
     // Request header from passed URL to verify legitimacy
     // Grab statusCode and end request.
     app.head(urlParam, (req, res) => {
-      urlStatus = res.status
-      console.log(urlStatus)
-      // res.end()
       let end = res.end
 
       res.end  = () => {
@@ -52,18 +49,6 @@ app.get('/url/:urlParam(*)', (request, response) => {
       res.end = end
       res.end()
     })
-
-    // Move this into the head request above?
-    // if (urlStatus = 200) {
-      // // Save to db.urlBank if passed URL returns status 200
-      // lmao.save((error) => {
-        // if (error) {
-          // response.send('Unable to write to collection')
-        // }
-      // })
-      // console.log('pass')
-      // response.json({lmao})
-    // }
   } else {
     // If passed URL does not satisfy regEx, return error message.
     urlParam = 'unfunny url. http(s):// prefix required. check url and retry.'
